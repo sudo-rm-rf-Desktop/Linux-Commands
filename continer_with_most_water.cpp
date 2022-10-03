aec208d1f8c7e642cfdc49e591052e5f066d73de
rahul <rahul@avysh.com>
2022-10-04 00:43:54 +0530

    recursion

---
+++ continer_with_most_water.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+	#endif
+
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0; i<n; i++){
+		cin>>a[i];
+	}
+
+	int start = 0;
+	int end = n;
+	int ans = INT_MIN;
+	while(start != end-1){
+		
+	}
+	cout<<ans;
+}
